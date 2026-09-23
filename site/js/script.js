41f2b6084b40f5f941455d8fa0e0adfb98ee2080
Bladix99YT <maikon15489566@hotmail.com>
2026-09-23 18:09:13 -0400

    Adicionando primeira estrutura base do site

---
+++ site/js/script.js
@@ -0,0 +1,15 @@
+const links = document.querySelectorAll(".menu a");
+
+links.forEach((link) => {
+
+    link.addEventListener("click", () => {
+
+        links.forEach((item) => {
+            item.classList.remove("ativo");
+        });
+
+        link.classList.add("ativo");
+
+    });
+
+});
